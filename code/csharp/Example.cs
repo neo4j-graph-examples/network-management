08180a9501d437c938d520fc18ad475d63c2c425
Sandbox 🤖 <neo4j-oss-build@neotechnology.com>
2020-12-07 12:14:22 +0000

    Updating sandbox network-management

---
+++ code/csharp/Example.cs
@@ -0,0 +1,38 @@
+// install dotnet core on your system
+// dotnet new console -o .
+// dotnet add package Neo4j.Driver
+// paste in this code into Program.cs
+// dotnet run
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Neo4j.Driver;
+  
+namespace dotnet {
+  class Example {
+  static async Task Main() {
+    var driver = GraphDatabase.Driver("bolt://<HOST>:<BOLTPORT>", 
+                    AuthTokens.Basic("<USERNAME>", "<PASSWORD>"));
+
+    var cypherQuery =
+      @"
+      MATCH (dc:DataCenter {location: $location})-[:CONTAINS]->(r:Router)-[:ROUTES]->(i:Interface)
+      RETURN i.ip as ip
+      ";
+
+    var session = driver.AsyncSession(o => o.WithDatabase("neo4j"));
+    var result = await session.ReadTransactionAsync(async tx => {
+      var r = await tx.RunAsync(cypherQuery, 
+              new { location="Iceland, Rekjavik"});
+      return await r.ToListAsync();
+    });
+
+    await session?.CloseAsync();
+    foreach (var row in result)
+      Console.WriteLine(row["ip"].As<string>());
+	  
+    }
+  }
+}
---
+++ code/python/example.py
@@ -0,0 +1,23 @@
+# pip3 install neo4j-driver
+# python3 example.py
+
+from neo4j import GraphDatabase, basic_auth
+
+driver = GraphDatabase.driver(
+  "bolt://<HOST>:<BOLTPORT>", 
+  auth=basic_auth("<USERNAME>", "<PASSWORD>"))
+
+cypher_query = '''
+MATCH (dc:DataCenter {location: $location})-[:CONTAINS]->(r:Router)-[:ROUTES]->(i:Interface)
+RETURN i.ip as ip
+'''
+
+with driver.session(database="neo4j") as session:
+  results = session.read_transaction(
+    lambda tx: tx.run(cypher_query,
+      location="Iceland, Rekjavik").data())
+
+  for record in results:
+    print(record['ip'])
+
+driver.close()
