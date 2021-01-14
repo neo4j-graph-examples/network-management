092de8328057f3f550a3b66a85aa79f9a8318e8b
Sandbox 🤖 <neo4j-oss-build@neotechnology.com>
2021-01-14 15:38:56 +0000

    Updating sandbox network-management

--- code/python/example.py
+++ code/python/example.py
@@ -4,7 +4,7 @@
 from neo4j import GraphDatabase, basic_auth
 
 driver = GraphDatabase.driver(
-  "bolt://<HOST>:<BOLTPORT>", 
+  "bolt://<HOST>:<BOLTPORT>",
   auth=basic_auth("<USERNAME>", "<PASSWORD>"))
 
 cypher_query = '''
@@ -15,8 +15,7 @@ RETURN i.ip as ip
 with driver.session(database="neo4j") as session:
   results = session.read_transaction(
     lambda tx: tx.run(cypher_query,
-      location="Iceland, Rekjavik").data())
-
+                      location="Iceland, Rekjavik").data())
   for record in results:
     print(record['ip'])
 
