2bdc7c4ff00a7603c4a5793046fa55ca66b19bd5
Sandbox 🤖 <neo4j-oss-build@neotechnology.com>
2023-12-14 15:06:13 +0000

    Triggered by direct commit. Origin: https://github.com/neo4j-contrib/sandbox-code-updater/commit/06cf0cc68341e050e3b12a5a7889bb3fde287eba

--- code/python/example.py
+++ code/python/example.py
@@ -1,22 +1,20 @@
-# pip3 install neo4j-driver
+# pip3 install neo4j
 # python3 example.py
 
 from neo4j import GraphDatabase, basic_auth
-
-driver = GraphDatabase.driver(
-  "bolt://<HOST>:<BOLTPORT>",
-  auth=basic_auth("<USERNAME>", "<PASSWORD>"))
 
 cypher_query = '''
 MATCH (dc:DataCenter {location: $location})-[:CONTAINS]->(r:Router)-[:ROUTES]->(i:Interface)
 RETURN i.ip as ip
 '''
 
-with driver.session(database="neo4j") as session:
-  results = session.read_transaction(
-    lambda tx: tx.run(cypher_query,
-                      location="Iceland").data())
-  for record in results:
-    print(record['ip'])
-
-driver.close()
+with GraphDatabase.driver(
+    "neo4j://<HOST>:<BOLTPORT>",
+    auth=("<USERNAME>", "<PASSWORD>")
+) as driver:
+    result = driver.execute_query(
+        cypher_query,
+        location="Iceland",
+        database_="neo4j")
+    for record in result.records:
+        print(record['ip'])
